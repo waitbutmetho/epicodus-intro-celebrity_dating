c5ff4c69cc64402c38a03c6f66297e35df4d24fd
Blair Peterson and Joseph Karasek <blairchalmers@gmail.com>
2016-01-13 10:39:32 -0800

    add click event actions to the submit button, prompts that the button works

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,12 @@ $(document).ready(function() {
 
   // on the event submit button is clicked
   // use branching to determine which celebrity to show or hide
+  $('.btn').click(function(event) {
+    alert('your button click works');
 
+
+    // work around for error with submitting forms without a connect server
+    event.preventDefault();
+  });
 
 });
