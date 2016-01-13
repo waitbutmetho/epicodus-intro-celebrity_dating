d9dbb15a1c3600c341b0706feadae583f32460fd
Blair Peterson and Joseph Karasek <joseph.karasek@gmail.com>
2016-01-13 11:03:16 -0800

    add function to hide contents to rest of branches

--- js/scripts.js
+++ js/scripts.js
@@ -13,14 +13,20 @@ $(document).ready(function() {
       // prompt that you are not old enough
       alert('You are not old enough to date!');
     } else if (age > 16 && age < 25) {
+      // hide all previously revealed info
+      $('.allcelebrities').children().addClass('celebrityHidden');
       // show celebrity1
       $('.allcelebrities').children('.celebrity1').removeClass('celebrityHidden');
 
     } else if (age > 24 && age < 65) {
+      // hide all previously revealed info
+      $('.allcelebrities').children().addClass('celebrityHidden');
       // show celebrity2
       $('.allcelebrities').children('.celebrity2').removeClass('celebrityHidden');
 
     } else {
+      // hide all previously revealed info
+      $('.allcelebrities').children().addClass('celebrityHidden');
       // show celebrity3
       $('.allcelebrities').children('.celebrity3').removeClass('celebrityHidden');
 
